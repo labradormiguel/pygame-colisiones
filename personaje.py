f404ed9d60d0c81182ee57c103bb14247d281e69
Miguel <miguel@example.com>
2024-08-13 23:51:53 +0200

    dia 2, creando personaje

---
+++ personaje.py
@@ -0,0 +1,14 @@
+import pygame
+
+class Cubo:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.ancho = 50
+        self.alto = 50
+        self.velocidad = 1
+        self.color = 'red'
+        self.rect = pygame.Rect(self.x,self.y,self.ancho,self.alto)
+
+    def dibujar(self, ventana):
+        pygame.draw.rect(ventana, self.color, self.rect)
--- pygame1.py
+++ pygame1.py
@@ -1,4 +1,5 @@
 import pygame
+from personaje import Cubo
 
 #pip install pygame
 
@@ -8,6 +9,8 @@ VENTANA = pygame.display.set_mode([ANCHO,ALTO])
 
 jugando = True
 
+cubo = Cubo(100,100)
+
 while jugando:
     eventos = pygame.event.get()
 
@@ -15,6 +18,7 @@ while jugando:
         if evento.type == pygame.QUIT:
             jugando = False
 
+    cubo.dibujar(VENTANA)
     pygame.display.update()
 
 quit()
